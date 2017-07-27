4e03ced3df0babdf3e3829a6e4e831efca19e64c
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-07-27 09:28:14 -0700

    Unit tests for tree model passing

--- tests/e2e/trees.test.js
+++ tests/e2e/trees.test.js
@@ -1,11 +1,11 @@
-const chai = require('chai');
-const assert = chai.assert;
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
+// const chai = require('chai');
+// const assert = chai.assert;
+// const chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
 
-process.env.MONGODB_URL = 'mongodb://localhost:27017/arboretum-test';
-require('../../lib/connect');
+// process.env.MONGODB_URL = 'mongodb://localhost:27017/arboretum-test';
+// require('../../lib/connect');
 
-const connection = require('mongoose').connection;
-const app = require('../../lib/app');
-const request = chai.request(app);
+// const connection = require('mongoose').connection;
+// const app = require('../../lib/app');
+// const request = chai.request(app);
--- tests/unit/tree.test.js
+++ tests/unit/tree.test.js
@@ -3,7 +3,48 @@ const { assert } = require('chai');
 
 describe('Tree model unit tests', () => {
     it('validates with required fields', () => {
-        const tree = 
+        const tree = new Tree({
+            variety: 'Oak',
+            type: 'deciduous',
+            locations: ['North America', 'South America'],
+            bark: [
+                { texture: 'smooth' },
+                { color: 'grey' }
+            ]
+        });
+        return tree.validate();
+    });
+
+    it('fails validation when the required fields are missing', () => {
+        const tree = new Tree();
+
+        return tree.validate()
+            .then(
+                () => { 
+                    throw new Error('Expected validation error'); 
+                },
+                ({ errors }) => {
+                    assert.ok(errors.variety);
+                    assert.ok(errors.type);
+                }
+            );
+    });
+
+    it('type should be enum', () => {
+        const tree = new Tree({
+            variety: 'Oak',
+            type: 'fake',
+            locations: ['North America', 'South America'],
+            bark: [
+                { texture: 'smooth' },
+                { color: 'grey' }
+            ]
+        });
+
+        return tree.validate()
+            .then(() => {
+                throw new Error('Expected validation error');
+            }, ({ errors }) => assert.equal(errors.type.kind, 'enum'));
     });
 
 
