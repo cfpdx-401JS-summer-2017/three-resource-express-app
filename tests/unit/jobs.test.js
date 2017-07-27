b5215465928ebc0e380d8f7d6f9077dbbc8d82d8
christylaguardia <christinelaguardia@gmail.com>
2017-07-27 10:26:50 -0700

    added job model and unit tests

---
+++ tests/unit/jobs.test.js
@@ -0,0 +1,34 @@
+const { assert } = require('chai');
+const Job = require('../../lib/models/job');
+
+describe('Job model', () => {
+
+    it('validates with required fields', () => {
+        const job = new Job({
+            position: 'Web Developer',
+            company: 'Google, Inc.',
+            applied: true
+        });
+        
+        return job.validate();
+    });
+
+    it('validation fails without required fields', () => {
+        const job = new Job();
+
+        return job.validate()
+            .then(
+                () => { throw new Error('Expected validation error'); },
+                ({ errors }) => {
+                    assert.ok(errors.position);
+                    assert.ok(errors.company);
+                    assert.ok(errors.applied);
+                }
+            );
+    });
+
+    // it('validates ', () => {
+        
+    // });
+
+});
