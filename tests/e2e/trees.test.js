ebe894ca495753c8d8686fe58f14566d825d3e90
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-07-27 10:38:09 -0700

    save tree written to fail

--- tests/e2e/trees.test.js
+++ tests/e2e/trees.test.js
@@ -1,11 +1,55 @@
-// const chai = require('chai');
-// const assert = chai.assert;
-// const chaiHttp = require('chai-http');
-// chai.use(chaiHttp);
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
-// process.env.MONGODB_URL = 'mongodb://localhost:27017/arboretum-test';
-// require('../../lib/connect');
+process.env.MONGODB_URL = 'mongodb://localhost:27017/arboretum-test';
+require('../../lib/connect');
 
-// const connection = require('mongoose').connection;
-// const app = require('../../lib/app');
-// const request = chai.request(app);
+const connection = require('mongoose').connection;
+const app = require('../../lib/app');
+const request = chai.request(app);
+
+describe('REST API for trees', () => {
+
+    before(() => connection.dropDatabase());
+
+    const oak = {
+        variety: 'Oak',
+        type: 'deciduous'
+    };
+
+    const birch = {
+        variety: 'Birch',
+        type: 'deciduous'
+    };
+
+    const redwood = {
+        variety: 'Redwood',
+        type: 'coniferous'
+    };
+
+    function saveTree(tree) {
+        return request.post('/trees')
+            .send(tree)
+            .then(({ body }) => {
+                tree._id = body._id;
+                tree.__v = body.__v;
+                return tree;
+            });
+    }
+
+    it('saves a tree', () => {
+        return saveTree(oak)
+            .then(savedTree => {
+                assert.ok(savedTree._id);
+                assert.deepEqual(savedTree, oak);
+            });
+    });
+
+
+
+
+
+
+});
