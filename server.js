c96710e6d26a7ffd699a7380cbf4ed7887fb8754
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-01 17:47:00 -0700

    server.js now working properly

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const http = require('http');
-// this will cause mongoose to make the db connection
-require('./lib/connect');
 const app = require('./lib/app');
+const http = require('http');
+
+// this will cause mongoose to make the db connection
+const connect = require('./lib/connect');
+
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/hollywood';
+connect(dbUri);
 
 const server = http.createServer(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     // eslint-disable-next-line
