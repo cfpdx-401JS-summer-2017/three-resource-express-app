6a619cf998fb13d5e94b3e07da4be0f4aa1aff2b
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-02 12:09:14 -0700

    get all test passes for flowers

--- tests/e2e/flowers.test.js
+++ tests/e2e/flowers.test.js
@@ -9,12 +9,82 @@ describe('flowers api', () => {
     let token = null;
     before(() => db.getToken().then(t => token = t));
 
-    it('initial GET returns empty list', () => {
-        return request.get('/api/flowers')
+    let daisy = {
+        name: 'Daisy',
+        color: 'white'
+    };
+
+    let rose = {
+        name: 'Rose',
+        color: 'red'
+    };
+
+    let poppy = {
+        name: 'Poppy',
+        color: 'orange'
+    };
+
+    function saveFlower(flower) {
+        return request 
+            .post('/api/flowers')
             .set('Authorization', token)
-            .then(req => {
-                const flowers = req.body;
-                assert.deepEqual(flowers, []);
+            .send(flower)
+            .then(({ body }) => {
+                flower._id = body._id;
+                flower.__v = body.__v;
+                return body;
             });
+    }
+
+    it('saves a flower', () => {
+        return saveFlower(daisy)
+            .then(savedFlower => {
+                assert.ok(savedFlower._id);
+                assert.deepEqual(savedFlower, daisy);
+            });
+    });
+
+    it('GETs a flower if it exists', () => {
+        return request.get(`/api/flowers/${daisy._id}`)
+            .set('Authorization', token)
+            .then(res => res.body)
+            .then(flower => assert.deepEqual(flower, daisy));
+    });
+
+    it('returns 404 if flower does not exist', () => {
+        return request.get('/api/flowers/123412345567898765466676')
+            .set('Authorization', token)
+            .then(() => { throw new Error('received 200 code when should be 404'); },
+                ({ response }) => assert.ok(response.notFound && response.error));
+    });
+
+    it('GETs all flowers', () => {
+        return Promise.all([
+            saveFlower(rose),
+            saveFlower(poppy)
+        ])
+            .then(savedFlowers => {
+                rose = savedFlowers[0];
+                poppy = savedFlowers[1];
+            })
+            .then(() => request
+                .get('/api/flowers')
+                .set('Authorization', token)    
+            )
+            .then(res => res.body)
+            .then(res => res.sort((a,b) => {
+                if(a.name > b.name) return 1;
+                else if(a.name < b.name) return -1;
+            }))
+            .then(flowers => assert.deepEqual(flowers, [daisy, poppy, rose])
+            );
+    });
+
+
+    it('errors on validation failure', () => {
+        return saveFlower({})
+            .then(() => { throw new Error('expected failure'); },
+                () => { }
+            );
     });
 });
