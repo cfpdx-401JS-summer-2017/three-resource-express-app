5800c618a015d2d928bf53b41146b40cb6b4ba72
christylaguardia <christinelaguardia@gmail.com>
2017-08-03 12:03:47 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const http = require('http');
-require('./lib/connect');
+const connect = require('./lib/connect');
 const app = require('./lib/app');
+
+// const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/job-search';
+const dbUri = 'mongodb://localhost:27017/job-search';
+connect(dbUri);
+
+// connect(dbUri => {
+//     // eslint-disable-next-line
+//     console.log('connected to database');
+// });
+
 const server = http.createServer(app);
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     // eslint-disable-next-line
